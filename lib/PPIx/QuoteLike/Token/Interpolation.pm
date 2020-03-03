15d1e4914fe5c35c8e2451bf276a9f7a0d3ed881
Tom Wyant <wyant@cpan.org>
2020-03-03 13:36:48 -0500

    Version 0.009_02
    Set version to 0.009_02. Update Changes file for changes since v00.009_01.

--- lib/PPIx/QuoteLike/Token/Interpolation.pm
+++ lib/PPIx/QuoteLike/Token/Interpolation.pm
@@ -18,7 +18,7 @@ use PPIx::QuoteLike::Utils qw{ __variables };
 
 use base qw{ PPIx::QuoteLike::Token };
 
-our $VERSION = '0.009_01';
+our $VERSION = '0.009_02';
 
 sub ppi {
     my ( $self ) = @_;
