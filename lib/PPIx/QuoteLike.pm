a57c00c236fd10966f66ee8f345f76ef73cc8a1e
Tom Wyant <wyant@cpan.org>
2021-01-29 05:08:56 -0500

    Version 0.014_02
    Set version to 0.014_02. Update Changes file for changes since v00.014_01.

--- lib/PPIx/QuoteLike.pm
+++ lib/PPIx/QuoteLike.pm
@@ -40,7 +40,7 @@ use PPIx::QuoteLike::Utils qw{
 use Scalar::Util ();
 use Text::Tabs ();
 
-our $VERSION = '0.014_01';
+our $VERSION = '0.014_02';
 
 use constant CLASS_CONTROL       => 'PPIx::QuoteLike::Token::Control';
 use constant CLASS_DELIMITER     => 'PPIx::QuoteLike::Token::Delimiter';
--- lib/PPIx/QuoteLike/Token/Interpolation.pm
+++ lib/PPIx/QuoteLike/Token/Interpolation.pm
@@ -20,7 +20,7 @@ use PPIx::QuoteLike::Utils qw{
 
 use base qw{ PPIx::QuoteLike::Token };
 
-our $VERSION = '0.014_01';
+our $VERSION = '0.014_02';
 
 sub ppi {
     my ( $self ) = @_;
