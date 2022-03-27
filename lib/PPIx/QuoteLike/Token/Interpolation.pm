dfa25a325efbb816b930607f4ee816ac787597b1
Tom Wyant <wyant@cpan.org>
2022-03-27 16:19:32 -0400

    Version 0.020_03
    Set version to 0.020_03. Update Changes file for changes since v00.020_02.

--- lib/PPIx/QuoteLike/Token/Interpolation.pm
+++ lib/PPIx/QuoteLike/Token/Interpolation.pm
@@ -20,7 +20,7 @@ use PPIx::QuoteLike::Utils qw{
 
 use base qw{ PPIx::QuoteLike::Token };
 
-our $VERSION = '0.020_02';
+our $VERSION = '0.020_03';
 
 sub ppi {
     my ( $self ) = @_;
--- lib/PPIx/QuoteLike/Token/Whitespace.pm
+++ lib/PPIx/QuoteLike/Token/Whitespace.pm
@@ -9,7 +9,7 @@ use base qw{ PPIx::QuoteLike::Token };
 
 use PPIx::QuoteLike::Constant qw{ @CARP_NOT };
 
-our $VERSION = '0.020_02';
+our $VERSION = '0.020_03';
 
 sub significant {
     return 0;
