8898fbf4a46980b50de0921f29b9a2149312d8d4
Tom Wyant <wyant@cpan.org>
2022-04-03 09:26:04 -0400

    Version 0.021
    Set version to 0.021. Update Changes file for changes since v00.020_03.

--- lib/PPIx/QuoteLike/Token/Interpolation.pm
+++ lib/PPIx/QuoteLike/Token/Interpolation.pm
@@ -20,7 +20,7 @@ use PPIx::QuoteLike::Utils qw{
 
 use base qw{ PPIx::QuoteLike::Token };
 
-our $VERSION = '0.020_03';
+our $VERSION = '0.021';
 
 sub ppi {
     my ( $self ) = @_;
--- lib/PPIx/QuoteLike/Utils.pm
+++ lib/PPIx/QuoteLike/Utils.pm
@@ -39,7 +39,7 @@ our @EXPORT_OK = qw{
     __variables
 };
 
-our $VERSION = '0.020_03';
+our $VERSION = '0.021';
 
 # Readonly::Scalar my $BRACED_RE	=> __match_enclosed( LEFT_CURLY );
 Readonly::Scalar my $BRACKETED_RE	=> __match_enclosed( '[' ); # ]
